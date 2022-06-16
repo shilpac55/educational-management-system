c3f08abf2d683fa79170435cd537953933e48e01
shilpa <shilpacyd@gmail.com>
2022-06-17 01:34:57 +0530

    student address added

--- student-mngt/src/main/java/com/shilpa/studentmngt/entity/Student.java
+++ student-mngt/src/main/java/com/shilpa/studentmngt/entity/Student.java
@@ -1,21 +1,26 @@
 package com.shilpa.studentmngt.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.Date;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Table(name= "students")
 public class Student {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="student_id")
 	public long studentId;
 	
 	@Column(name="student_name")
@@ -35,86 +40,20 @@ public class Student {
 	
 	@Column(name="student_email")
 	public String studentEmail;
-	//public Address studentAddress;
-	//public Course course;
+
 	
 	@Column(name="student_aadhar")
 	public long studentAadhar;
 	
 	@Column(name="father_aadhar")
-	public long fathderAadhar;
-	
-	public Student() {};
-	
-	public Student(long studentId, String studentName, String studentFatherName, String studentMotherName,
-			Date studentDOB, String studentGender, String studentEmail, long studentAadhar, long fathderAadhar) {
-		super();
-		this.studentId = studentId;
-		this.studentName = studentName;
-		this.studentFatherName = studentFatherName;
-		this.studentMotherName = studentMotherName;
-		this.studentDOB = studentDOB;
-		this.studentGender = studentGender;
-		this.studentEmail = studentEmail;
-		this.studentAadhar = studentAadhar;
-		this.fathderAadhar = fathderAadhar;
-	}
-	public long getStudentId() {
-		return studentId;
-	}
-	public void setStudentId(long studentId) {
-		this.studentId = studentId;
-	}
-	public String getStudentName() {
-		return studentName;
-	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	public String getStudentFatherName() {
-		return studentFatherName;
-	}
-	public void setStudentFatherName(String studentFatherName) {
-		this.studentFatherName = studentFatherName;
-	}
-	public String getStudentMotherName() {
-		return studentMotherName;
-	}
-	public void setStudentMotherName(String studentMotherName) {
-		this.studentMotherName = studentMotherName;
-	}
-	public Date getStudentDOB() {
-		return studentDOB;
-	}
-	public void setStudentDOB(Date studentDOB) {
-		this.studentDOB = studentDOB;
-	}
-	public String getStudentGender() {
-		return studentGender;
-	}
-	public void setStudentGender(String studentGender) {
-		this.studentGender = studentGender;
-	}
-	public String getStudentEmail() {
-		return studentEmail;
-	}
-	public void setStudentEmail(String studentEmail) {
-		this.studentEmail = studentEmail;
-	}
-	public long getStudentAadhar() {
-		return studentAadhar;
-	}
-	public void setStudentAadhar(long studentAadhar) {
-		this.studentAadhar = studentAadhar;
-	}
-	public long getFathderAadhar() {
-		return fathderAadhar;
-	}
-	public void setFathderAadhar(long fathderAadhar) {
-		this.fathderAadhar = fathderAadhar;
-	}
-	
-	
-	
+	public long fatherAadhar;
+
+	//public Address studentAddress;
+
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="address_id", referencedColumnName = "address_id")
+	public Address address;
+
+	//public Course course;
 	
 }
